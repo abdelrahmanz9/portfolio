4470403fef2436084a814f3cca43c90d785d7909
abdelrahman <abdelrahmansaadz9a@gmail.com>
2022-10-30 05:21:52 +0200

    first commit

---
+++ assets/custom.js
@@ -0,0 +1,18 @@
+
+
+$(document).ready(
+    function(){
+        $('.slide').slideUp(500)
+    }
+)
+
+
+// $(function() {  
+//     $("html").niceScroll({
+//         zindex: "999",
+//         scrollspeed: 600
+//     });
+// });
+
+
+
